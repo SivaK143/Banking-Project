2f49a2c2e90cc371587e9df43068ee8018634a62
Lokesh <mandla.lokeshnaidu@gmail.com>
2026-08-03 19:38:06 +0530

    service creation

---
+++ account-service/src/main/java/com/banking/accountservice/entity/AccountStatus.java
@@ -0,0 +1,10 @@
+package com.banking.accountservice.entity;
+
+/**
+ * Account Lifecycle Status.
+ */
+public enum AccountStatus {
+    ACTIVE,
+    BLOCKED,
+    CLOSED
+}
---
+++ account-service/src/main/java/com/banking/accountservice/repository/AccountRepository.java
@@ -0,0 +1,12 @@
+package com.banking.accountservice.repository;
+
+import com.banking.accountservice.entity.Account;
+import com.banking.accountservice.entity.AccountType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AccountRepository extends JpaRepository<Account, String> {
+
+    boolean existsByEmail(String email);
+}
