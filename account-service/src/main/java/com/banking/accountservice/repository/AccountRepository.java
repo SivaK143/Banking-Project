7c70abc08c7e2eae4e7ebf4e8bd39bc9eef37d3f
Lokesh <mandla.lokeshnaidu@gmail.com>
2026-08-16 21:13:36 +0530

    account Service

--- account-service/src/main/java/com/banking/accountservice/repository/AccountRepository.java
+++ account-service/src/main/java/com/banking/accountservice/repository/AccountRepository.java
@@ -4,9 +4,12 @@ import com.banking.accountservice.entity.Account;
 import com.banking.accountservice.entity.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AccountRepository extends JpaRepository<Account, String> {
 
     boolean existsByEmail(String email);
+    boolean existsByAccountNumber(String accountNumber);
+    Optional<Account> findByAccountNumber(String accountNumber);
 }
--- account-service/src/main/java/com/banking/accountservice/service/AccountService.java
+++ account-service/src/main/java/com/banking/accountservice/service/AccountService.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.security.SecureRandom;
 
 @Service
 @Slf4j
@@ -18,6 +19,7 @@ import java.math.BigDecimal;
 public class AccountService {
 
     private final AccountRepository accountRepository;
+    private SecureRandom secureRandom = new SecureRandom();
 
     public AccountResponse createAccount(CreateAccountRequest request){
         log.info("Creating account for: {}", request.getEmail());
@@ -45,7 +47,51 @@ public class AccountService {
         return mapToResponse(savedAccount);
     }
 
+    /**
+     * Get account by account Number
+     * @param accountNumber
+     * @return
+     */
+    public AccountResponse getAccount(String accountNumber){
+        Account account = accountRepository.findByAccountNumber(accountNumber)
+                .orElseThrow(()-> new RuntimeException("Account Not found"));
+        return mapToResponse(account);
+    }
 
+    /**
+     * Get account Balance
+     * @param accountNumber
+     * @return
+     */
+    public BigDecimal getBalance(String accountNumber){
+        Account account = accountRepository.findByAccountNumber(accountNumber)
+                .orElseThrow(()-> new RuntimeException("Account Not found"));
+        return account.getBalance();
+    }
+
+    /**
+     * Block account - Called by fraud detection service via Kafka
+     * @param accountNumber
+     */
+    public void blockAccount(String accountNumber){
+        log.info("Blocking account: {}", accountNumber);
+        Account account = accountRepository.findByAccountNumber(accountNumber)
+                .orElseThrow(()-> new RuntimeException("Account Not found"));
+        account.setStatus(AccountStatus.BLOCKED);
+        accountRepository.save(account);
+        log.info("Account Blocked: {}",accountNumber);
+    }
+
+    //Generate unique 12digit account Number
+    private String generateAccountNumber(){
+
+        String accountNumber;
+        do {
+            long number = secureRandom.nextLong(1_000_000_000_000L);
+            accountNumber = String.format("%012d", number);
+        }while(accountRepository.existsByAccountNumber(accountNumber));
+        return accountNumber;
+    }
     private AccountResponse mapToResponse(Account account){
         AccountResponse response = new AccountResponse();
         response.setId(account.getId());
